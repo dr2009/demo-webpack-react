320881fd0e9f3a6f1bf700daef21770f8893dda2
of2502 <yangxingzhou@qianmi.com>
2017-03-03 16:50:56 +0800

    数据透传 connect

--- src/components/App.js
+++ src/components/App.js
@@ -5,10 +5,45 @@ import './App.css';
 
 import Counter from './Counter';
 
+const mapStateToProps = state => ({
+    counter: state.counter,
+    restCounter: 100 - state.counter
+});
+
+@connect(mapStateToProps)
+class Inner extends React.Component {
+    render() {
+        const {counter, restCounter} = this.props;
+        return (
+            <div>
+                {counter}
+                +
+                {restCounter}
+                =
+                {counter + restCounter}
+                <br/>
+            </div>
+        );
+    }
+}
+
+class Middle extends React.Component {
+    render() {
+        return (
+            <div>
+                {this.props.children}
+            </div>
+        );
+    }
+}
+
 const App = ({counter, actions}) => (
     <div className="app">
         <h2>React Counter!</h2>
         <Counter />
+        <Middle>
+            <Inner />
+        </Middle>
     </div>
 );
 
